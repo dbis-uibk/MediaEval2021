62db495ec88fe664a0931e566676129b12e87a5f
Michael Vötter <mikevo-uibk@famv.net>
2021-07-28 16:10:59 +0200

    Add vgg-ish model implementation

---
+++ src/mediaeval2021/models/ensemble.py
@@ -0,0 +1,54 @@
+"""Module contains ensemble models."""
+import numpy as np
+from sklearn.base import BaseEstimator
+from sklearn.base import ClassifierMixin
+from sklearn.base import clone
+
+
+class Ensemble(BaseEstimator, ClassifierMixin):
+    """Splits label prediction to multiple classifiers."""
+
+    def __init__(self, base_estimator, label_splits, epochs=None):
+        """Creates the object."""
+        self.base_estimator = base_estimator
+        self.label_splits = label_splits
+        self.epochs = epochs
+
+        self.models = []
+
+    def fit(self, data, labels, epochs=None):
+        """Fits the classifiers."""
+        if len(self.models) == 0:
+            for split in self.label_splits:
+                split_model = clone(self.base_estimator)
+                if self.epochs is not None:
+                    split_model.epochs = self.epochs
+                split_model.label_split = split
+                self.models.append(split_model)
+
+        for model, split in zip(self.models, self.label_splits):
+            if epochs is not None:
+                model.fit(data, labels[..., split], epochs=epochs)
+            else:
+                model.fit(data, labels[..., split])
+
+    def predict(self, data):
+        """Joins prediction of all classifiers."""
+        return self._ensamble_predict(data, prediction_type='label')
+
+    def predict_proba(self, data):
+        """Joins prediction of all classifiers."""
+        return self._ensamble_predict(data, prediction_type='proba')
+
+    def _ensamble_predict(self, data, prediction_type):
+        predictions = []
+        for model in self.models:
+            if prediction_type == 'proba':
+                predictions.append(model.predict_proba(data))
+            elif prediction_type == 'label':
+                predictions.append(model.predict(data))
+
+        predictions = np.concatenate(predictions, axis=1)
+
+        label_order = np.concatenate(self.label_splits, axis=0)
+        return predictions[..., np.argsort(label_order)]
---
+++ src/mediaeval2021/models/vggish.py
@@ -0,0 +1,106 @@
+"""This module contains the implementation of VGG-ish models."""
+from sklearn.base import BaseEstimator
+from sklearn.base import ClassifierMixin
+from skorch import NeuralNetClassifier
+import torch.nn as nn
+
+
+class VGGishBaseline(BaseEstimator, ClassifierMixin):
+    """The VGGish baseline wrapt as a sklearn classifier."""
+
+    def __init__(self, epochs=10):
+        """Creates the model."""
+        self._model = NeuralNetClassifier(
+            CNN(num_class=10),
+            max_epochs=epochs,
+            lr=0.1,
+            iterator_train__shuffle=True,
+        )
+
+    def fit(self, features, target, epochs=None):
+        """Fits the model for a given number of epochs."""
+        self._model.fit(features, target)
+
+    def validate(self, features, target):
+        """Validates the model."""
+
+    def predict(self, features):
+        """Returns the classes predicted by the model."""
+        self._model.predict(features)
+
+    def predict_proba(self, features):
+        """Returns the class probabilities predicted by the model."""
+        self._model.predict_proba(features)
+
+
+class CNN(nn.Module):
+    """The VGGish baseline model used since the MediaEval 2019.
+
+    The implementation is taken from the provided github repository.
+    https://github.com/MTG/mtg-jamendo-dataset/blob/master/scripts/baseline/model.py
+    """
+
+    def __init__(self, num_class=15):
+        """Creates the pytorch model."""
+        super(CNN, self).__init__()
+
+        # init bn
+        self.bn_init = nn.BatchNorm2d(1)
+
+        # layer 1
+        self.conv_1 = nn.Conv2d(1, 64, 3, padding=1)
+        self.bn_1 = nn.BatchNorm2d(64)
+        self.mp_1 = nn.MaxPool2d((2, 4))
+
+        # layer 2
+        self.conv_2 = nn.Conv2d(64, 128, 3, padding=1)
+        self.bn_2 = nn.BatchNorm2d(128)
+        self.mp_2 = nn.MaxPool2d((2, 4))
+
+        # layer 3
+        self.conv_3 = nn.Conv2d(128, 128, 3, padding=1)
+        self.bn_3 = nn.BatchNorm2d(128)
+        self.mp_3 = nn.MaxPool2d((2, 4))
+
+        # layer 4
+        self.conv_4 = nn.Conv2d(128, 128, 3, padding=1)
+        self.bn_4 = nn.BatchNorm2d(128)
+        self.mp_4 = nn.MaxPool2d((3, 5))
+
+        # layer 5
+        self.conv_5 = nn.Conv2d(128, 64, 3, padding=1)
+        self.bn_5 = nn.BatchNorm2d(64)
+        self.mp_5 = nn.MaxPool2d((4, 4))
+
+        # classifier
+        self.dense = nn.Linear(64, num_class)
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        """Computation performed by the model."""
+        x = x.unsqueeze(1)
+
+        # init bn
+        x = self.bn_init(x)
+
+        # layer 1
+        x = self.mp_1(nn.ELU()(self.bn_1(self.conv_1(x))))
+
+        # layer 2
+        x = self.mp_2(nn.ELU()(self.bn_2(self.conv_2(x))))
+
+        # layer 3
+        x = self.mp_3(nn.ELU()(self.bn_3(self.conv_3(x))))
+
+        # layer 4
+        x = self.mp_4(nn.ELU()(self.bn_4(self.conv_4(x))))
+
+        # layer 5
+        x = self.mp_5(nn.ELU()(self.bn_5(self.conv_5(x))))
+
+        # classifier
+        x = x.view(x.size(0), -1)
+        x = self.dropout(x)
+        logit = nn.Sigmoid()(self.dense(x))
+
+        return logit
