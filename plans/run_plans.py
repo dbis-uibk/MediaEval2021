7e45e710593bdfa36fb0f5ddc12dfd04a952caef
Andreas Peintner <andreas.peintner@uibk.ac.at>
2021-10-27 10:20:50 +0200

    fix bug

--- plans/run_plans.py
+++ plans/run_plans.py
@@ -1,6 +1,6 @@
 import os
 
-for filename in os.listdir('./'):
+for filename in os.listdir('plans/'):
     if filename.startswith("fixed") and filename.endswith(".py"): 
          print(filename)
          os.system(f"sbatch --job-name=mediaeval-vggish --mail-user=andreas.peintner@uibk.ac.at --time=8:00:00 --mem=250G ~/jobs/single-node-titan.job 'plans/{filename}'")
